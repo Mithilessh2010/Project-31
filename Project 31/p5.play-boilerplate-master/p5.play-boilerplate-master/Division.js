84fed50159724c9307090d58cb7281e591ed774e
Mithilessh2010 <71537184+Mithilessh2010@users.noreply.github.com>
2020-11-20 22:47:35 -0800

    Add files via upload

---
+++ Project 31/p5.play-boilerplate-master/p5.play-boilerplate-master/Division.js
@@ -0,0 +1,17 @@
+class Disvisions {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic = true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.width, this.height);
+    }
+}
